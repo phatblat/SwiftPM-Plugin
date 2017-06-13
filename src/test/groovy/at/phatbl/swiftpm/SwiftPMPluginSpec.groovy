894e1af8c9e0b2db7a0706eebf56555780bb8996
Ben Chatelain <ben@octop.ad>
2017-06-12 18:00:14 -0600

    Add spock framework

---
+++ src/test/groovy/at/phatbl/swiftpm/SwiftPMPluginSpec.groovy
@@ -0,0 +1,10 @@
+package at.phatbl.swiftpm
+
+import spock.lang.Specification
+
+class SwiftPMPluginSpec extends Specification {
+    def ""() {
+        expect:
+        Math.max(1, 2) == 2
+    }
+}
