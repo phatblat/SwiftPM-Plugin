57f7d28f9cc65ac285834a3a941c3ececf898fc8
Ben Chatelain <ben@octop.ad>
2017-06-13 09:12:07 -0600

    Add example features

--- src/test/groovy/at/phatbl/swiftpm/SwiftPMPluginSpec.groovy
+++ src/test/groovy/at/phatbl/swiftpm/SwiftPMPluginSpec.groovy
@@ -3,8 +3,26 @@ package at.phatbl.swiftpm
 import spock.lang.Specification
 
 class SwiftPMPluginSpec extends Specification {
-    def ""() {
+    def "when-then"() {
+        when:
+        def x = Math.max(1, 2)
+
+        then:
+        x == 2
+    }
+
+    def "expect"() {
         expect:
         Math.max(1, 2) == 2
+    }
+
+    def "computing the maximum of two numbers"() {
+        expect:
+        Math.max(a, b) == c
+
+        where:
+        a << [5, 3]
+        b << [1, 9]
+        c << [5, 9]
     }
 }
