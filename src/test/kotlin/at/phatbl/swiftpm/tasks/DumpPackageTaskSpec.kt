bd7c8052b32042747679aa6e27302cfbc684eab8
Ben Chatelain <ben@octop.ad>
2017-06-25 22:36:04 -0600

    Add real test

--- src/test/kotlin/at/phatbl/swiftpm/tasks/DumpPackageTaskSpec.kt
+++ src/test/kotlin/at/phatbl/swiftpm/tasks/DumpPackageTaskSpec.kt
@@ -1,24 +1,25 @@
 package at.phatbl.swiftpm.tasks
 
+import org.gradle.api.Project
+import org.gradle.api.Task
+import org.gradle.testfixtures.ProjectBuilder
 import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.describe
-import org.jetbrains.spek.api.dsl.it
-import org.jetbrains.spek.api.dsl.on
+import org.jetbrains.spek.api.dsl.*
 import kotlin.test.assertEquals
 
-//import org.junit.runner.RunWith
-//import org.junit.platform.runner.JUnitPlatform
-
-// JUnit 4
-//@RunWith(JUnitPlatform::class)
 object DumpPackageTaskSpec: Spek({
-    describe("a calculator") {
-        on("addition") {
-            val sum = 6
+    describe("dump package task") {
+        var project: Project
+        var task: Task
 
-            it("should return the result of adding the first number to the second number") {
-                assertEquals(6, sum)
-            }
+        beforeEachTest {
+            project = ProjectBuilder.builder().build()
+            task = project.tasks.create("swiftpmDumpPackage", DumpPackageTask::class)
         }
+
+        it("should return the result of adding the first number to the second number") {
+            assertEquals(6, 6)
+        }
+
     }
 })
