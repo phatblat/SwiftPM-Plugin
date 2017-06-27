e4a3fd4fb51bf3e2903ab2a3dc4a52fee0df41cd
Ben Chatelain <ben@octop.ad>
2017-06-26 21:04:07 -0600

    WIP: Check task creation
    Issue: => org.gradle.api.GradleException: Could not generate a proxy class for class at.phatbl.swiftpm.tasks.DumpPackageTask.

--- src/test/kotlin/at/phatbl/swiftpm/tasks/DumpPackageTaskSpec.kt
+++ src/test/kotlin/at/phatbl/swiftpm/tasks/DumpPackageTaskSpec.kt
@@ -5,12 +5,13 @@ import org.gradle.api.Task
 import org.gradle.testfixtures.ProjectBuilder
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.*
-import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
 
 object DumpPackageTaskSpec: Spek({
     describe("dump package task") {
-        var project: Project
-        var task: Task
+        var project = ProjectBuilder.builder().build()
+        var task = project.tasks.create("swiftpmDumpPackage", DumpPackageTask::class.java)
 
         beforeEachTest {
             project = ProjectBuilder.builder().build()
@@ -18,7 +19,8 @@ object DumpPackageTaskSpec: Spek({
         }
 
         it("can be added to project") {
-            assertEquals(6, 6)
+            assertNotNull(task)
+            assertTrue { task is DumpPackageTask }
         }
     }
 })
