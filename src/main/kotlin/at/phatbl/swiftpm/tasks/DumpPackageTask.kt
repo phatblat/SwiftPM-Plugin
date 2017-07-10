b5e490f387a3fc76d02561c07244e5306075d49d
Ben Chatelain <ben@octop.ad>
2017-07-10 07:25:24 -0600

    Convert Constants to kotlin

--- src/main/kotlin/at/phatbl/swiftpm/tasks/DumpPackageTask.kt
+++ src/main/kotlin/at/phatbl/swiftpm/tasks/DumpPackageTask.kt
@@ -1,10 +1,10 @@
 package at.phatbl.swiftpm.tasks
 
+import at.phatbl.swiftpm.Constants.Companion.SWIFTPM_TASK_GROUP
+
 // TODO: Figure out how to extend AbstractExecTask groovy class
 //import at.phatbl.swiftpm.tasks.AbstractExecTask
 import org.gradle.api.tasks.Exec
-
-import at.phatbl.swiftpm.Constants.*
 
 open class DumpPackageTask : Exec() {
     init {
