f682c98d0da4f2ecfed4239e5adb99abcd9de2bc
Ben Chatelain <ben@octop.ad>
2018-03-31 20:03:16 -0600

    🚨 Remove unused instance variables
    https://codebeat.co/projects/github-com-phatblat-swiftpm-plugin-master/pull_requests/113290
    
    Itroduced issues
    - [info] Assignment Branch Condition too high:   SwiftPMPlugin.at.phatbl.swiftpm.apply
    - [critical]Too many instance variables: 11 instance variables

--- src/main/kotlin/at/phatbl/swiftpm/SwiftPMPlugin.kt
+++ src/main/kotlin/at/phatbl/swiftpm/SwiftPMPlugin.kt
@@ -16,37 +16,24 @@ import at.phatbl.swiftpm.Constants.Companion.TASK_VERSION
 import at.phatbl.swiftpm.tasks.*
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.Task
 
 class SwiftPMPlugin : Plugin<Project> {
     lateinit var project: Project
-
-    lateinit var build: Task
-    lateinit var test: Task
-    lateinit var version: Task
-
-    lateinit var clean: Task
-    lateinit var describe: Task
-    lateinit var dumpPackage: Task
-    lateinit var generateXcodeTask: Task
-    lateinit var pmVersion: Task
-    lateinit var reset: Task
-    lateinit var toolsVersion: Task
 
     override fun apply(project: Project) {
         this.project = project
         project.logger.info("SwiftPM plugin version: ${Version.text}")
 
-        build = project.tasks.create(TASK_SWIFT_BUILD, BuildTask::class.java)
-        test = project.tasks.create(TASK_SWIFT_TEST, TestTask::class.java)
-        version = project.tasks.create(TASK_SWIFT_VERSION, VersionTask::class.java)
+        project.tasks.create(TASK_SWIFT_BUILD, BuildTask::class.java)
+        project.tasks.create(TASK_SWIFT_TEST, TestTask::class.java)
+        project.tasks.create(TASK_SWIFT_VERSION, VersionTask::class.java)
 
-        clean = project.tasks.create(TASK_CLEAN, CleanTask::class.java)
-        describe = project.tasks.create(TASK_DESCRIBE, DescribeTask::class.java)
-        dumpPackage = project.tasks.create(TASK_DUMP_PACKAGE, DumpPackageTask::class.java)
-        generateXcodeTask = project.tasks.create(TASK_GENERATE_XCODE_PROJECT, GenerateXcodeTask::class.java)
-        pmVersion = project.tasks.create(TASK_VERSION, PMVersionTask::class.java)
-        reset = project.tasks.create(TASK_RESET, ResetTask::class.java)
-        toolsVersion = project.tasks.create(TASK_TOOLS_VERSION, ToolsVersionTask::class.java)
+        project.tasks.create(TASK_CLEAN, CleanTask::class.java)
+        project.tasks.create(TASK_DESCRIBE, DescribeTask::class.java)
+        project.tasks.create(TASK_DUMP_PACKAGE, DumpPackageTask::class.java)
+        project.tasks.create(TASK_GENERATE_XCODE_PROJECT, GenerateXcodeTask::class.java)
+        project.tasks.create(TASK_VERSION, PMVersionTask::class.java)
+        project.tasks.create(TASK_RESET, ResetTask::class.java)
+        project.tasks.create(TASK_TOOLS_VERSION, ToolsVersionTask::class.java)
     }
 }
